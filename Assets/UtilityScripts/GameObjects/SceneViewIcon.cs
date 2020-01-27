8c3eb63d7d809f3779fcd5035ca2d598e587f081
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2020-01-27 12:37:50 +0500

    combined templates

---
+++ Assets/UtilityScripts/GameObjects/SceneViewIcon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneViewIcon : MonoBehaviour
+{
+
+	public Color iconColor = Color.yellow;
+	public float radius = 0.1f;
+
+	void OnDrawGizmos()
+	{
+		// Draw a yellow sphere at the transform's position
+		Gizmos.color = iconColor;
+		Gizmos.DrawSphere(transform.position,radius);
+	}
+}
---
+++ Assets/UtilityScripts/General/VectorExtenstions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorExtensions
+{
+	public static Vector3 ScaleByValue(this Vector3 obj, float n)
+	{
+		return obj *= n;
+	}
+
+	public static Vector3 down(this Vector3 obj)
+	{
+		return -Vector3.up;
+	}
+
+	public static Vector3 left(this Vector3 obj)
+	{
+		return -Vector3.right;
+	}
+
+	public static Vector3 backward(this Vector3 obj)
+	{
+		return -Vector3.forward;
+	}
+}
