52728b288c378d2a0fdf8888bc9ea9534adb1446
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2020-02-03 16:04:27 +0500

    Added Transform Tools, Unity Little Helper (ULH) and Project Prospector

---
+++ Assets/Plugins/TransformTools/Editor/AlignTools.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Linq;
+
+public static class AlignTools
+{
+    public enum AlignMode { avg, min, max };
+    public enum AlignAxis { x, y, z };
+    public static Transform[] selected;
+
+    public static void AlignAverage(AlignAxis _axis)
+    {
+        if (!Selection.activeTransform) return;
+        selected = Selection.transforms;
+        float total = 0;
+        foreach (var trans in selected)
+        {
+            switch (_axis)
+            {
+                case AlignAxis.x:
+                    total += trans.position.x;
+                break;
+                case AlignAxis.y:
+                    total += trans.position.y;
+                break;
+                case AlignAxis.z:
+                    total += trans.position.z;
+                break;
+            }
+        }
+        float avg = total / selected.Length;
+        
+        Undo.RecordObjects(Selection.transforms, "Align Average by " + _axis.ToString().ToUpper());
+        foreach (var trans in selected)
+        {
+            switch(_axis)
+            {
+                case AlignAxis.x:
+                    trans.position = new Vector3(avg, trans.position.y, trans.position.z);
+                break;
+                case AlignAxis.y:
+                    trans.position = new Vector3(trans.position.x, avg, trans.position.z);
+                    break;
+                case AlignAxis.z:
+                    trans.position = new Vector3(trans.position.x, trans.position.y, avg);
+                break;
+            }
+        }
+    }
+
+    public static void Align(AlignAxis _axis, AlignMode _mode)
+    {
+        if (!Selection.activeTransform) return;
+
+        switch (_axis)
+        {
+            case AlignAxis.x:
+                selected = Selection.transforms.OrderBy(tr => tr.position.x).ToArray();
+            break;
+            case AlignAxis.y:
+                selected = Selection.transforms.OrderBy(tr => tr.position.y).ToArray();
+            break;
+            case AlignAxis.z:
+                selected = Selection.transforms.OrderBy(tr => tr.position.z).ToArray();
+            break;
+        }
+
+        Vector3 targetPos = Vector3.zero;
+
+        switch (_mode)
+        {
+            case AlignMode.min:
+                targetPos = selected[0].position;
+            break;
+
+            case AlignMode.max:
+                targetPos = selected[selected.Length-1].position;
+                break;
+        }
+
+        Undo.RecordObjects(Selection.transforms, "Align " + _mode.ToString().ToUpper() + " by " + _axis.ToString().ToUpper());
+
+        foreach (var trans in selected)
+        {
+            switch (_axis)
+            {
+                case AlignAxis.x:
+                    trans.position = new Vector3(targetPos.x, trans.position.y, trans.position.z);
+                break;
+                case AlignAxis.y:
+                    trans.position = new Vector3(trans.position.x, targetPos.y, trans.position.z);
+                    break;
+                case AlignAxis.z:
+                    trans.position = new Vector3(trans.position.x, trans.position.y, targetPos.z);
+                    break;
+            }
+            
+        }
+    }
+
+    
+}
+
---
+++ Assets/Plugins/TransformTools/Editor/RandomTools.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using System.Linq;
+
+public class RandomTools
+{
+    public static void RandomPosition(int _axis, float _min, float _max)
+    {
+        if (!Selection.activeTransform) { Debug.Log("No selection"); return; }
+        var selectedTransforms = Selection.transforms;
+        Undo.RecordObjects(Selection.transforms, "Random Position");
+
+        foreach (var trans in selectedTransforms)
+        {
+            Vector3 pos = trans.localPosition;
+            float rand = Random.Range(_min, _max);
+
+            if (_axis == 0) trans.localPosition = new Vector3(rand, pos.y, pos.z);
+            if (_axis == 1) trans.localPosition = new Vector3(pos.x, rand, pos.z);
+            if (_axis == 2) trans.localPosition = new Vector3(pos.x, pos.y, rand);
+            if (_axis == 3) trans.localPosition = 
+                new Vector3(Random.Range(_min, _max), Random.Range(_min, _max), Random.Range(_min, _max));
+        }
+    }
+
+    public static void RandomRotation(int _axis, float _min, float _max)
+    {
+        if (!Selection.activeTransform) return;
+        var selectedTransforms = Selection.transforms;
+        Undo.RecordObjects(Selection.transforms, "Random Rotation");
+
+        foreach (var trans in selectedTransforms)
+        {
+            Vector3 rot = trans.eulerAngles;
+
+            if (_axis == 0) rot.x = Random.Range(0, 360f);
+            if (_axis == 1) rot.y = Random.Range(0, 360f);
+            if (_axis == 2) rot.z = Random.Range(0, 360f);
+            if (_axis == 3) rot =
+                new Vector3(Random.Range(_min, _max), Random.Range(_min, _max), Random.Range(_min, _max));
+
+            trans.eulerAngles = rot;
+        }
+    }
+
+    public static void RandomScale(int _axis, float _min, float _max)
+    {
+        if (!Selection.activeTransform) return;
+        var selectedTransforms = Selection.transforms;
+        Undo.RecordObjects(Selection.transforms, "Random Scale");
+        foreach (var trans in selectedTransforms)
+        {
+
+            Vector3 scale = trans.localScale;
+            float rand = System.Math.Abs(Random.Range(_min, _max));
+
+            if (_axis == 0) scale = new Vector3(rand, scale.y, scale.z);
+            if (_axis == 1) scale = new Vector3(scale.x, rand, scale.z);
+            if (_axis == 2) scale = new Vector3(scale.x, scale.y, rand);
+            if (_axis == 3) scale =
+                new Vector3(System.Math.Abs(Random.Range(_min, _max)), System.Math.Abs(Random.Range(_min, _max)), System.Math.Abs(Random.Range(_min, _max)));
+
+            trans.localScale = scale;
+        }
+
+    }
+}
