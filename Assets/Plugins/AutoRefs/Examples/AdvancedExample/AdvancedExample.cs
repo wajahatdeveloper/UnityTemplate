d963afce3533fde60cc016262cfcd0926558171c
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2020-02-11 12:57:13 +0500

    Added Auto Ref Plugin

---
+++ Assets/Plugins/AutoRefs/Examples/AdvancedExample/AdvancedExample.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvancedExample : MonoBehaviour
+{
+	[AutoRef(AutoRefTargetType.Self)]
+	[SerializeField]
+	TestRef m_SelfTestRef;
+
+	[AutoRef(AutoRefTargetType.Parent)]
+	[SerializeField]
+	TestRef m_ParentTestRef;
+
+	[AutoRef(AutoRefTargetType.Children)]
+	[SerializeField]
+	TestRef m_ChildTestRef;
+
+	[AutoRef(AutoRefTargetType.Children)]
+	[SerializeField]
+	TestRef[] m_ChildTestRefArray;
+
+	[AutoRef(AutoRefTargetType.Self | AutoRefTargetType.Children)]
+	[SerializeField]
+	TestRef[] m_SelfAndChildTestRefArray;
+
+	[AutoRef(AutoRefTargetType.Siblings)]
+	[SerializeField]
+	TestRef[] m_SiblingTestRefArray;
+
+	[AutoRef(AutoRefTargetType.Scene)]
+	[SerializeField]
+	TestRef[] m_SceneTestRefArray;
+
+	[AutoRef(AutoRefTargetType.Self | AutoRefTargetType.Parent | AutoRefTargetType.Children | AutoRefTargetType.Siblings)]
+	[SerializeField]
+	List<TestRef> m_SelfParentChildAndSiblingTestRefList;
+}
